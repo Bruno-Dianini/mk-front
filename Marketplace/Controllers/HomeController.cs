1bf30ba95922ba46c4fdaf1363cd28a8caddd00d
Bruno <bdianini@gmail.com>
2023-11-19 14:42:07 -0300

    front

---
+++ Marketplace/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Marketplace.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Marketplace.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult cadastro()
+        {
+            return View();
+        }
+        public IActionResult recuperacao()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
